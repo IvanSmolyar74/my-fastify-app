279e534a43c50d47af5eb62ff513539eb1c0622c
IvanSmolyar74 <ivansmolar09001@gmail.com>
2025-02-15 17:29:04 +0400

    Update server port configuration to use environment variable

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,15 @@ app.register(setRoutes);
 export const mongo = new MongoService("mongodb+srv://ivansmolar09001:BujL5AxCU5Zod1uo@cluster0.bqzwf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 const start = () => {
-  app.listen({ port: 3000 }, (err, address) => {
-      if (err) {
-        app.log.error(err);
-        process.exit(1);
-      } else {
-        app.log.info(`Server listening at ${address}`);
-        mongo.connect()
-      }
-    });
+  app.listen({ port: Number(process.env.PORT) || 3000 }, (err, address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    } else {
+      app.log.info(`Server listening at ${address}`);
+      mongo.connect();
+    }
+  });
 };
 
 start();
